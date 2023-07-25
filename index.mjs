20109c39d6ec66ccaedf596d590159597b3d6399
ammar-rasheed@hotmail.com <ammar-rasheed@hotmail.com>
2023-07-25 16:19:22 +0300

    Use process.env.PORT for server port

--- index.mjs
+++ index.mjs
@@ -110,7 +110,7 @@ app.use(express.static('public'));
 
 
 
-        const port = process.env.PORT || 3000;
+        const port = 3000;
 
         app.listen(port, () => {
             console.log(`Server is running on port ${port}`);
